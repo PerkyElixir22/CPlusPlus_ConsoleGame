bce5564ee07161f9d0734ce3fa48a96337947a87
PerkyElixir22 <perkyelixir22@gmail.com>
2021-01-24 17:49:28 +0100

    Update setGameVariables.h

--- setGameVariables.h
+++ setGameVariables.h
@@ -5,3 +5,4 @@ int loadRoom; // The next room that should be loaded
 int money; // amount of money the player has
 fish Fish[8]; // data for the fishes the player has
 bankAccount bank[8]; // put max amount of bank accounts at 8 because that feels like a good amount
+shopItem fishCatchers[8]; // define the items that will get us some items that we need for getting fish
